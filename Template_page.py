d07864e0650f62f9686b12e8b26961ecb1c1b75a
Bercerfk57 <126180928+Bercerfk57@users.noreply.github.com>
2024-04-09 17:16:26 +0200

    petit ménage et changment de nom

--- Template_page.py
+++ Template_page.py
@@ -2,8 +2,8 @@ from tkinter import *
 from PIL import Image, ImageTk
 
 # constants/path
-path_img_bg = "Static/background.png"
-path_img_fg = "Static/joliecarre.png"
+path_img_bg = "Static/Bg_Bapt'ss.png"
+path_img_fg = "Static/Cadre_Bapt'ss.png"
 
 ratio_square = 1.5
 
