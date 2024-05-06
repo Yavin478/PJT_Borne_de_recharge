6cbc0e971d6691e8ee311cf9def8daa18cc633ab
Bercerfk57 <126180928+Bercerfk57@users.noreply.github.com>
2024-05-06 18:42:40 +0200

    Squashed commit of the following:
    commit 8934cd1979557f2d867d57c178e40b2ffdd39f0b
    Author: Yavin <evan.leroux@yahoo.fr>
    Date:   Mon May 6 18:28:26 2024 +0200
    
        MAJ Lydia : 06/05/2024
    
    commit da87441bfc7db524e6b41c19ba12a908d0c26dc1
    Author: Yavin <evan.leroux@yahoo.fr>
    Date:   Sun May 5 17:28:05 2024 +0200
    
        Lydia update : 05/05/2024
    
    commit 7a3897190e272596a2fc903cb8a134272c15b22c
    Author: Yavin <evan.leroux@yahoo.fr>
    Date:   Sun May 5 17:27:45 2024 +0200
    
        Lydia update : 05/05/2024
    
    commit 55dc7edf7f227536ddb0690efbbdb1c745f8a9e5
    Author: Yavin478 <126158870+Yavin478@users.noreply.github.com>
    Date:   Sat May 4 21:31:31 2024 +0200
    
        Rename API_Lydia.py to API_lydia.py
    
    commit ed622dab05d25d9b667315ab525e7ffcfdf89c0c
    Author: Yavin <evan.leroux@yahoo.fr>
    Date:   Fri May 3 19:29:36 2024 +0200
    
        Lydia update : 03/05/2024
    
    commit 468f8308c25db3194a66820b7401425a91660635
    Author: Yavin <evan.leroux@yahoo.fr>
    Date:   Fri May 3 19:19:37 2024 +0200
    
        Lydia update : 03/05/2024
        Trie et suppression du code
    
    commit ea7cc9f8f3080eb7b6378864ba09cc1afc6a2b38
    Author: Yavin <evan.leroux@yahoo.fr>
    Date:   Sun Apr 28 19:14:46 2024 +0200
    
        Lydia update : 28/04/2024
    
    commit c894716783fdfe951d89e2a886058dbe8d00a21f
    Author: Yavin <evan.leroux@yahoo.fr>
    Date:   Sun Apr 28 18:56:57 2024 +0200
    
        Lydia update : 28/04/2024
    
    commit afca1d2bfebfeac1c7e8ee14732342f4b54b7690
    Author: Yavin <evan.leroux@yahoo.fr>
    Date:   Sun Apr 28 18:12:40 2024 +0200
    
        Lydia update : 28/04/2024
    
    commit ce3c2ef802c02514bbe449795ad383266c8fd4bb
    Author: Yavin <evan.leroux@yahoo.fr>
    Date:   Sun Apr 28 18:06:05 2024 +0200
    
        RFID Test : 28/04/2024
    
    commit 6ff75c6bbbbb077b2ed7549442a20f721bc06cc4
    Merge: 28929bc f5096dc
    Author: Yavin <evan.leroux@yahoo.fr>
    Date:   Thu Apr 11 17:56:37 2024 +0200
    
        Merge branch 'main' into Lydia

---
+++ API_lydia.py
@@ -0,0 +1,56 @@
+print("Demarrage 'API_lydia.py'")
+
+#### Fonction de vérification d'une transaction lydia ####
+
+def Lydia_check(token_public,montant,phone,order_id,Qrcode):
+    # L'URL de l'API pour initier une transaction (remplacer par l'URL de test ou de production selon le cas)
+    #url = "https://lydia-app.com/api/payment/payment.json" # Production
+    url = "https://homologation.lydia-app.com/api/payment/payment.json" # Test
+
+    #Convertit les données scannées du qrcode en un format comprehensible pour la requête Json
+    paymentData = json.dumps(Qrcode)
+
+    # Les données à envoyer à l'API
+    data = {
+        'vendor_token': token_public,
+        'amount': montant,
+        'phone': phone,
+        'order_id': order_id,
+        'paymentData': paymentData,
+        'currency': 'EUR'
+    }
+
+    # Configuration optionnelle : désactiver la vérification SSL pour cet exemple.
+    # En production, assurez-vous que SSL est activé et correctement configuré.
+    requests.packages.urllib3.disable_warnings()
+
+    # Effectuer la requête POST
+    response = requests.post(url, data=data, verify=False)
+
+    # Vérifier la réponse
+    if response.status_code == 200:
+        # Convertir la réponse en JSON
+        response_data = response.json()
+
+        if response_data['error'] == "0":
+            print("Transaction réussie.")
+            print("Identifiant de la transaction :", response_data['transaction_identifier'])
+            return response_data['transaction_identifier']
+        else:
+            print("Erreur lors de la transaction :", response_data['error'], response_data['message'])
+            return None
+
+    else:
+        print("Erreur de requête HTTP :", response.status_code)
+        return None
+
+
+'''
+## Test de la fonction lydia check
+token_public = "660e5b8b4c353994613407"
+phone='33782977418'
+montant=4
+order_id=1
+Qrcode=["SLebMnW8pk9MlSRO2RYvv04BOw/Eq2mdi21fhEQeEkeC7mYjUzhvAer6XWnuCrTRU6pKsLc7Dxe8hdSo07YhZQ8g5zGHwhB8xo4tKKD0hQvC6EQHCUXJo3SRlmRKn913Ar40gWe2v31OIdbipJcAzTtZW7V3KxPbu5OLRh4epWU=","3"]
+transaction_identifier=Lydia_check(token_public,montant,phone,order_id,Qrcode)
+'''
