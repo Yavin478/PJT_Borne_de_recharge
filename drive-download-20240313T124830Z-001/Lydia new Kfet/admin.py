b9888408aca5a6dd4fa9f3c0b9e3ba8613ddcc3e
Bercerfk57 <126180928+Bercerfk57@users.noreply.github.com>
2024-05-06 19:23:27 +0200

    nettoyage

--- drive-download-20240313T124830Z-001/Lydia new Kfet/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import RechargeLydia
-
-# Register your models here.
-
-#@admin.register(RechargeLydia)
-class AdminRechargeLydia(admin.ModelAdmin):
-    list_display = ("cible_recharge", "date", "montant")
