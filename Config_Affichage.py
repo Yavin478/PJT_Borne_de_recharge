80a914c6de084d055e045a9a6cac6882017fe00c
Yavin <evan.leroux@yahoo.fr>
2024-05-09 16:50:12 +0200

    Update main-OrangePI: Ajout de la lecture du montant sur la carte ou son ajout dans la BDD si nouvelle (09/05/2024)

--- Config_Affichage.py
+++ Config_Affichage.py
@@ -25,7 +25,7 @@ txt_indic= {"terminée":"Très bon gala!",
             "error_QR":"Recommencez avec un QRcode \n Lydia Valide",
             "error_rezal":"veuillez contacter un membre du Rezal",
             "error_carte":"Recommencez avec une carte valide",
-            "error_montant":"Recommencez avec un montant \n valide < "+str(config.maxTransaction/100)+"€ \n Le montant total de la carte ne doit pas être > à "+str(config.maxMontant/100)+" €" }
+            "error_montant":"Recommencez avec un montant\n valide < "+str(config.maxTransaction/100)+"€ \n Le montant total de la carte ne doit pas être\n > à "+str(config.maxMontant/100)+" €" }
 
 txt_esc="Appuyez sur ESC pour annuler"
 txt_montant_in="Vous avez: \n"
