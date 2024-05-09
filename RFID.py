9e403494768fbc49d9c63772d6e64f0276228f6c
Bercerfk57 <126180928+Bercerfk57@users.noreply.github.com>
2024-05-09 15:52:46 +0200

    omg

--- RFID.py
+++ RFID.py
@@ -184,6 +184,7 @@ def RFID_getUID(master):
             if status == MIFAREReader.MI_OK:
                 uidstring = STRING_Tag(uid, len(uid))
                 master.Check_Carte(uidstring)
+                return None
         master.after(400,RFID_getUID,master)
     except:
         print("PROBLEME LECTURE UID")
