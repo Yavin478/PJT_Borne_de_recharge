d1d72e360086d0577e5fc66049912c3e6d335450
Yavin <evan.leroux@yahoo.fr>
2024-05-14 22:13:21 +0200

    Ajout des ping pour les serveurs Guinche et google pour la vérification des connections

--- LOG.py
+++ LOG.py
@@ -10,7 +10,7 @@ def LOG_add(fichierName,contenu):
 def Entrer_log (projet_path,nom_fichier_log,contenu):   # Toutes les variables en str
     os.makedirs(nom_fichier_log, exist_ok=True)
     now=ctime()
-    LOG_add(projet_path+'/'+nom_fichier_log," at: "+str(now)+" "+contenu+"\n")
+    LOG_add(projet_path+'/'+nom_fichier_log,"At: "+str(now)+":"+contenu+"\n")
 
 #####Exemple d'entrée de log.txt
 #Entrer_log(os.path.abspath(os.path.dirname(__file__)),'LOG.txt','self.mode=='+'carte'+' heure: '+str(datetime.datetime.now())+"\n")
