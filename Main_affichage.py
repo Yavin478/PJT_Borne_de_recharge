a0b6790d038abbca8b9d4a29ea1f9daa6b1a12a9
Yavin <evan.leroux@yahoo.fr>
2024-05-14 20:57:39 +0200

    Ajout des ping pour les serveurs Guinche et google pour la vérification des connections

--- Main_affichage.py
+++ Main_affichage.py
@@ -117,6 +117,7 @@ class MainApp(Tk):
         if len(SQL_SELECT(QUERRY_getCarte(self.UID))) == 0:  # test si la carte est déjà présente dans la bdd
             SQL_EXECUTE(QUERRY_addCarte(self.UID))
 
+        print("BDD pas ok")
         self.argent = SQL_SELECT(QUERRY_getMoney(self.UID))[0][0] / 100  # Pour convertir le montant en euros
 
         self.mode = "Montant"
