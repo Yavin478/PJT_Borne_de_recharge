0419325a893bf834b2231c870189bc8ed05561e2
Yavin <evan.leroux@yahoo.fr>
2024-05-14 20:03:15 +0200

    Ajout des ping pour les serveurs Guinche et google pour la vérification des connections

--- Main_affichage.py
+++ Main_affichage.py
@@ -16,7 +16,7 @@ class MainApp(Tk):
     def Carte_test(self):
         self.L_presence_card.append(RFID_presence())
         if len(self.L_presence_card) > 3:
-            print("Liste :",L_presence_card)
+            print("Liste :",self.L_presence_card)
             if not (True in self.L_presence_card[1:]):
                 print("No RFID presence")
                 if self.mode=="Finish":
