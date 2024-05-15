6198b1fdd182d4994a97723d2c955f05ea21d41d
Yavin <evan.leroux@yahoo.fr>
2024-05-16 00:21:16 +0200

    Passage en mode production : rajout des lignes d'erreur pour les vraies test

--- config.py
+++ config.py
@@ -46,7 +46,7 @@ class config:  # Définition des variables reliée à l'objet config définissan
     # Montant en centimes minimal à pouvoir mettre pendant une transaction
     minMontant = 100
     # Montant max à pouvoir être mis sur une carte
-    maxTransaction = 5000
+    maxTransaction = 9900
     # Montant en centime maximal à pouvoir être contenu sur une carte
     maxMontant = 15000
 
