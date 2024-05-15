377499d0eb3f939cbac9bc48cd1f69f4f441aaba
Bercerfk57 <126180928+Bercerfk57@users.noreply.github.com>
2024-05-15 23:33:50 +0200

    maj

--- USB.py
+++ USB.py
@@ -1,29 +1,34 @@
 print("Demarrage 'USB.py'")
 from DATA import *
 
-def get_device_id(device_name):
+
+def get_device_ids(device_name):
     if device_name == 'keyboard':
         device_name="Barcode Reader"
     else:
         device_name="BF SCAN SCAN KEYBOARD"
     try:
+        # Exécuter la commande xinput list
         result = subprocess.run(['xinput', 'list'], capture_output=True, text=True, check=True)
+        # Filtrer la sortie pour obtenir les IDs des périphériques
         lines = result.stdout.split('\n')
+        device_ids = []
         for line in lines:
             if device_name in line:
+                # Utiliser split pour extraire l'ID du périphérique
                 parts = line.split('id=')
                 if len(parts) > 1:
                     id_part = parts[1]
                     device_id = id_part.split()[0]
-                    return device_id
-        return None
+                    device_ids.append(device_id)
+        return device_ids
     except subprocess.CalledProcessError as e:
         print(f"Error executing command: {e}")
-        return None
+        return []
 
 
 def command_usb(usb,type):
-    id=get_device_id(usb)
+    id=get_device_ids(usb)
     print(id)
     if id==None:
         return False
