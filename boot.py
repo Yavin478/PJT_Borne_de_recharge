6df567617e5f0bd8226f67a57c9534d76a6367b6
Yavin <evan.leroux@yahoo.fr>
2024-05-16 02:11:01 +0200

    Passage en mode production : rajout des lignes d'erreur pour les vraies test

--- boot.py
+++ boot.py
@@ -6,10 +6,12 @@ from Main_affichage import *
 def is_rotation_complete():
     # Cette fonction doit être adaptée à la manière dont vous vérifiez l'état de la rotation
     result = subprocess.run(["xrandr"], capture_output=True, text=True)
+    print("Résultat :",result)
     return "right" in result.stdout  # Ajustez cette condition selon votre cas
 
 # Attendre que la rotation soit complète
 while not is_rotation_complete():
+    print("time")
     time.sleep(1)  # Attend une seconde avant de vérifier à nouveau
 
 # Le reste de votre script
