32bb1f10ebd13d478e2644c733965c1c55390ff5
Yavin <evan.leroux@yahoo.fr>
2024-05-17 00:33:29 +0200

    Fix bug RFID

--- boot.py
+++ boot.py
@@ -2,10 +2,13 @@ print("Démarrage 'boot.py'")
 
 from Main_affichage import *
 
-Entrer_log(setting.projet_path, "Logs", "Démarage Programe")
+Entrer_log(setting.projet_path, "Logs_prg", "Démarage Programe")
 
-root=MainApp()
-root.mainloop()
+# root=MainApp()
+# root.mainloop()
+
+while True:
+    print("Test presence :",RFID_presence())
 
 if config.debugging :
     print("Attente de 3s")
