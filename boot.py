9743bd605804a3c9b6d58eaf03beb49d6ff8ddd7
Yavin <evan.leroux@yahoo.fr>
2024-05-16 02:04:47 +0200

    Passage en mode production : rajout des lignes d'erreur pour les vraies test

--- boot.py
+++ boot.py
@@ -21,13 +21,3 @@ root.mainloop()
 if config.debugging :
     print("Attente de 3s")
     sleep(3)
-
-
-
-"""    while True:  # Boucle infinie du script
-    try:
-        exec(open(projet_path+'PJT_Borne_de_recharge/loop_borne_auto.py').read())  # Execution du script se répétant jusqu'à l'arrêt du système
-    except:
-        print("Erreur de boot")
-        exec(open(projet_path+'PICONFLEX2000-CLIENT/error.py').read())  # Script de gestion et affichage des erreurs
-"""
