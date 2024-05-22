179ab07674db4e9aab7b27eb6d9dd400d6979546
Yavin <evan.leroux@yahoo.fr>
2024-05-22 17:13:18 +0200

    Modif de config lydia pour repasser en mode développement pour les essais vidéo de la borne pour le pjt

--- Main_affichage.py
+++ Main_affichage.py
@@ -171,7 +171,7 @@ class MainApp(Tk):
 
     def QR_transact(self, uid):
         self.UID_check=STRING_uidStrToInt(uid)
-        print("UID de la carte :";self.UID_check)
+        print("UID de la carte :",self.UID_check)
         Entrer_log(setting.projet_path, "Logs_prg", "Carte trouvé: "+str(self.UID_check))
 
         if self.UID_check!=self.UID:
