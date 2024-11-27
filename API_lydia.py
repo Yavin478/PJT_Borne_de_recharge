838fdd81c84f8f61e75bcef53c7988616b84149d
Yavin478 <126158870+Yavin478@users.noreply.github.com>
2024-11-27 23:21:31 +0100

    Update API_lydia.py

--- API_lydia.py
+++ API_lydia.py
@@ -36,8 +36,8 @@ def Lydia_check(token_public,montant,phone,order_id,Qrcode):
                 Entrer_log(setting.projet_path, "Logs_prg", "Identifiant de la transaction :"+ str(response_data['transaction_identifier']))
                 return response_data['transaction_identifier']
             else : 
-                Entrer_log(setting.projet_path, "Logs_error","L'erreur suivante est survenu lors de la transaction :" + str(response_data['error']))
-                #Entrer_log(setting.projet_path, "Logs_error","L'erreur suivante est survenu lors de la transaction :" + str(response_data['status']) +"  :  " + str(response_data['message']))
+                Entrer_log(setting.projet_path, "Logs_error","Code d'erreur survenu lors de la transaction :" + str(response_data['error']))
+                Entrer_log(setting.projet_path, "Logs_error","Message d'erreur associé :" + str(response_data['status']) +"  :  " + str(response_data['message']))
                 return None
         except :
             Entrer_log(setting.projet_path, "Logs_error","L'erreur suivante est survenu lors de la transaction :" + str(response_data['status']) +"  :  " + str(response_data['message']))
